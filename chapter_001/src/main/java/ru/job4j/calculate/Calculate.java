ac2c0673487ea9bf929fa24de4d6a48ba084bb0a
zloblya2018 <zloblya2018@mail.ru>
2018-09-16 21:57:46 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,25 @@
 package ru.job4j.calculate;
-
+/**
+* Calculate.
+*
+* @author Belyakov Maksim (mailto: zloblya2018@mail.ru)
+* @version $Id$
+* @since 16.09.2018
+*/
 public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+     public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+     }
 }
