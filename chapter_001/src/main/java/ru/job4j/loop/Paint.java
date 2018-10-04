ec827ac969ad7b831f8fa2823fe5710d475267cf
Беляков Максим <zloblya2018@mail.ru>
2018-10-04 11:10:54 +0300

    5.4. Построить пирамиду в псевдографике (Правильный вариант).

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,7 +1,4 @@
 package ru.job4j.loop;
-
-import java.util.function.BiPredicate;
-
 /**
  * Pyramid.
  * @author Belyakov Maksim (zloblya2018@mail.ru)
@@ -9,35 +6,43 @@ import java.util.function.BiPredicate;
  * @since 03.10.2018
  */
 public class Paint {
-        public String rightTrl(int height) {
-        return this.loopBy(
-                height,
-                height,
-                (row, column) -> row >= column
-        );
+    public String rightTrl(int height) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int weight = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= column) {
+                    stringBuilder.append("^");
+                } else {
+                    stringBuilder.append(" ");
+                }
+            }
+            stringBuilder.append(System.lineSeparator());
+        }
+        return stringBuilder.toString();
     }
 
     public String leftTrl(int height) {
-        return this.loopBy(
-                height,
-                height,
-                (row, column) -> row >= height - column - 1
-        );
-    }
-
-    public String pyramid(int height) {
-        return this.loopBy(
-                height,
-                2 * height - 1,
-                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
-        );
-    }
-
-    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
-        StringBuilder screen = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
+        int weight = height;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != weight; column++) {
-                if (predict.test(row, column)) {
+                if (row >= weight - column - 1) {
+                    stringBuilder.append("^");
+                } else {
+                    stringBuilder.append(" ");
+                }
+            }
+            stringBuilder.append(System.lineSeparator());
+        }
+        return stringBuilder.toString();
+    }
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
