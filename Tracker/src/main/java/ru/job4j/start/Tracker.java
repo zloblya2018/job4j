b3e286380fdb2641e79d964392558c316062f10c
Беляков Максим <zloblya2018@mail.ru>
2018-11-08 01:29:00 +0300

    ООП Инкапсуляция недописанный метод delete

--- Tracker/src/main/java/ru/job4j/start/Tracker.java
+++ Tracker/src/main/java/ru/job4j/start/Tracker.java
@@ -3,17 +3,58 @@ package ru.job4j.start;
 import ru.job4j.models.*;
 import java.util.Random;
 
+/**
+ * В этом классе хранятся все заявки.
+ * Возможно добавлять, редактировать и удалять заявки, также
+ * возможен вывод списка всех заявок и вывод списка по фильтру.
+ */
+
 public class Tracker {
-    private Item[] items = new Item[10];
+    private Item[] items = new Item[100];
     private int position = 0;
     private static final Random RN = new Random();
-
+    /**
+     * Добавление заявок.
+     */
     public Item add(Item item) {
         item.setId(String.valueOf(RN.nextInt()));
         this.items[position++] = item;
         return item;
     }
-
+    /**
+     * Редактирование заявок.
+     */
+    public Item replace(String id, Item item) {
+        int position = 0;
+        for (Item itemIndex : items) {
+            for (int index = 0; index != this.position; index++) {
+                if (itemIndex != null && itemIndex.getId().equals(id)) {
+                    position = index;
+                    break;
+                }
+            }
+        }
+        this.items[position] = item;
+        return this.items[position];
+    }
+    /**
+     * Удаление заявок.
+     */
+    public void delete(String id) {
+        int position = 0;
+        for (Item itemIndex : items) {
+            for (int index = 0; index != this.position; index++) {
+                if (itemIndex != null && itemIndex.getId().equals(id)) {
+                    position = index;
+                    break;
+                }
+            }
+            this.items[position] = null;
+        }
+    }
+    /**
+     * Получение заявки.
+     */
     protected Item findById(String id) {
         Item result = null;
         for (Item item : items) {
@@ -24,14 +65,24 @@ public class Tracker {
         }
         return result;
     }
+    /**
+     * Получение списка по имени.
+     */
+    public Item[] findByName(String key) {
+        Item[] result = null;
+
+        return result;
+    }
 
     String generateId() {
         return String.valueOf(System.currentTimeMillis() + RN.nextInt());
     }
-
+    /**
+     * Получение списка всех заявок.
+     */
     public Item[] getAll() {
-        Item[] result = new Item[position];
-        for(int index = 0; index != position; index++) {
+        Item[] result = new Item[this.position];
+        for(int index = 0; index != this.position; index++) {
             result[index] = this.items[index];
         }
         return result;
