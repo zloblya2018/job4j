15cf76fd6e0d8f1f395e95e86ef9e68a2dce179e
Беляков Максим <zloblya2018@mail.ru>
2019-02-06 19:13:47 +0300

    3. if-else-throw [#49895]

--- chapter_002/src/main/java/ru/job4j/tracker/start/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/start/StubInput.java
@@ -17,18 +17,6 @@ public class StubInput implements Input {
 
     @Override
     public int ask(String question, List<Integer> range) {
-        int key = Integer.valueOf(this.ask(question));
-        boolean exist = false;
-        for (int value : range) {
-            if (value == key) {
-                exist = true;
-                break;
-            }
-        }
-        if (exist) {
-            return key;
-        } else {
-            throw new MenuOutExeption("Выйти обратно в меню?");
-        }
+        return Integer.valueOf(this.answers[this.position++]);
     }
 }
